37b3e6b9416b39398de1b0708bcbf275011e368b
Kcyvx <gt78colo@gmail.com>
2025-10-17 14:16:18 +0200

    fix bug

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,9 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "react/no-unescaped-entities": "off", // 👈 ajoute juste cette ligne
+    },
   },
 ];
 
